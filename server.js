0398f565f54a2786ef2fb06ee4202e79e60d92c8
Alex22sv <ae22mp@gmail.com>
2024-10-14 22:00:44 -0600

    Temporarily remove edit button while working on it

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-
 const PORT = process.env.PORT || 3000;
 const baseURL = 'https://pokeapi.co/api/v2';
 const app = express();
